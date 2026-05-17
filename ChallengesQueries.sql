7ae68e3f16475702bf01e262db30260b2df855c1
Harsh Raundal <dkingston0000@gmail.com>
2026-05-17 17:21:32 +0530

    Create ChallengesQueries.sql
    Challenges and questions that I solved for practice, its all done on the data already uploaded in this repository.

---
+++ ChallengesQueries.sql
@@ -0,0 +1,8 @@
+select * from payment
+select amount, payment_date from payment order by amount asc
+
+
+select * from actor
+select first_name, last_name from actor order by last_name desc, last_name 
+
+select first_name, last_name from customer order by last_name desc, first_name desc
