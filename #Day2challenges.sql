4d7fd0dcf1e673eae3c7392a94028d7c163f73e8
Harsh Raundal <dkingston0000@gmail.com>
2026-05-18 14:27:21 +0530

    Add SQL queries for Day 2 challenges
    These are challenges and their solutions for me to learn Where keyword in sql

---
+++ #Day2challenges.sql
@@ -0,0 +1,8 @@
+practicing 'where' keyword:
+select distinct payment_id, customer_id from payment where staff_id = 1
+select first_name, last_name, last_update from actor where first_name = 'NICK'
+
+
+challenge:
+select first_name, last_name from customer where first_name = 'ERICA'
+select count(*) first_name from payment where customer_id = 100 
