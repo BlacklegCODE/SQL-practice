4405a25817ff5c20c3f8f656818250ff741b8e1c
Harsh Raundal <dkingston0000@gmail.com>
2026-05-18 22:21:18 +0530

    Update #Day2challenges.sql

--- #Day2challenges.sql
+++ #Day2challenges.sql
@@ -6,3 +6,8 @@ select first_name, last_name, last_update from actor where first_name = 'NICK'
 challenge:
 select first_name, last_name from customer where first_name = 'ERICA'
 select count(*) first_name from payment where customer_id = 100 
+
+operators:
+select count(*) return_date from rental where return_date is null
+
+select payment_id, amount from payment where amount <= 2
