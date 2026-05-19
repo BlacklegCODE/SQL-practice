15b3a58985f20325a4b0ad3ebee9257435b4bd12
Harsh Raundal <dkingston0000@gmail.com>
2026-05-19 16:21:53 +0530

    Update #Day2challenges.sql

--- #Day2challenges.sql
+++ #Day2challenges.sql
@@ -1,13 +1,13 @@
-practicing 'where' keyword:
+--practicing 'where' keyword:
 select distinct payment_id, customer_id from payment where staff_id = 1
 select first_name, last_name, last_update from actor where first_name = 'NICK'
 
 
-challenge:
+--challenge:
 select first_name, last_name from customer where first_name = 'ERICA'
 select count(*) first_name from payment where customer_id = 100 
 
-operators:
+--operators:
 select count(*) return_date from rental where return_date is null
 
 select payment_id, amount from payment where amount <= 2
