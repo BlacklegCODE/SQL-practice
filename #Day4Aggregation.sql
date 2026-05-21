224c6ad8cc7327999aa8ba96a8f7339e88480860
Harsh Raundal <dkingston0000@gmail.com>
2026-05-21 23:31:16 +0530

    Add query for film replacement cost statistics

--- #Day4Aggregation.sql
+++ #Day4Aggregation.sql
@@ -3,3 +3,12 @@ select sum(amount) as total,
 round(avg(amount),3) as average,
 max(amount) as maximum, 
 min(amount) as minimum from payment as average
+
+
+--Day 4 challenge:
+--Challenge:Manager wants total, average, min, max of the replacement costs of the films from the film data.
+
+select sum(replacement_cost) as total_cost,
+max(replacement_cost) maximum_cost ,
+round(avg(replacement_cost),2) as average_cost,
+min(replacement_cost) as minimum_cost from film
