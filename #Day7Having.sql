8d7ee83f52138095533ea876858cb0f5eb996460
Harsh Raundal <dkingston0000@gmail.com>
2026-05-24 16:15:02 +0530

    Update #Day7Having.sql

--- #Day7Having.sql
+++ #Day7Having.sql
@@ -8,3 +8,16 @@ order by count(*) desc
 
 --Challenge question for 'Having':
 
+-- In 2020-4-28,29,30 were days with very high revenue.
+-- Thats why we want to focus in this task only on these days
+
+-- Find out what is the average payment amount grouped by
+-- customer and day, consider only the days/customers with
+-- more than one payment, per customer and day
+-- order by the average amount in a descending order
+
+select  customer_id, date(payment_date), round(avg(amount),2)as average , count(*) from payment 
+where  payment_date between '2020-4-28' and '2020-4-30'
+group by customer_id, date(payment_date)
+having count(*) > 1
+order by average desc
