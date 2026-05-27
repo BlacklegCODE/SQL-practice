b121b22f724a1099c563f0f9b844f411e5dc882d
Harsh Raundal <dkingston0000@gmail.com>
2026-05-27 22:15:25 +0530

    Add SQL queries for string manipulation on customer data
    Added SQL queries to extract specific characters from customer names and email addresses.

--- #Day9LeftRight.sql
+++ #Day9LeftRight.sql
@@ -1,3 +1,14 @@
 --Learning left and right
 select right(left(first_name, 2),1), first_name
 from customer 
+
+
+--Extract last 5 characters of the emailaddress
+--the email address always ends with '.org'
+--how can you extract jus the dot '.' from the email address?
+
+select right(email,5) from customer
+
+--now third:
+
+select left(right(email,4),1), email from customer
