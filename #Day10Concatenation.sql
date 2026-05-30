62aff0556301cfa9314191ec27e112209892ed79
Harsh Raundal <dkingston0000@gmail.com>
2026-05-30 22:17:42 +0530

    Update #Day10Concatenation.sql

--- #Day10Concatenation.sql
+++ #Day10Concatenation.sql
@@ -4,3 +4,11 @@ select left(first_name, 1) || right(first_name, 3), first_name from customer
 --concatenation example 2
 select left(first_name, 1) ||'.'|| left(last_name, 1) || '--' as initials, 
 first_name from customer
+
+
+--Challenge query for concatenation
+--Anonymise the emails from customer table
+
+select left(email,1) ||'***'|| right(email, 19) as anonymised
+from customer
+
