0e2affef96462fe7cbdb26c97b1ab7f48320365f
Harsh Raundal <dkingston0000@gmail.com>
2026-05-30 22:13:30 +0530

    Update #Day10Concatenation.sql

--- #Day10Concatenation.sql
+++ #Day10Concatenation.sql
@@ -1,2 +1,6 @@
 --concatenation example
 select left(first_name, 1) || right(first_name, 3), first_name from customer
+
+--concatenation example 2
+select left(first_name, 1) ||'.'|| left(last_name, 1) || '--' as initials, 
+first_name from customer
