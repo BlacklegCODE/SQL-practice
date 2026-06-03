79fdac5884a12a53b509d3da0f0999d2bb9e454d
Harsh Raundal <dkingston0000@gmail.com>
2026-06-03 17:00:08 +0530

    Update #Day15CTEs.sql

--- #Day15CTEs.sql
+++ #Day15CTEs.sql
@@ -5,3 +5,10 @@ select * from
 (select city_id, max(country_id) as maximum from city group by country_id,city_id) as identities
 
 where maximum < 30
+
+--New Example:
+
+with mi as
+(select city_id, max(country_id) as maximum from city group by country_id,city_id)
+
+select * from mi where city_id >300 
