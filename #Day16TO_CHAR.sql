632040aa62d0bad4bfc1b7479ec398dd34c1e329
Harsh Raundal <dkingston0000@gmail.com>
2026-06-06 18:23:46 +0530

    Update #Day16TO_CHAR.sql

--- #Day16TO_CHAR.sql
+++ #Day16TO_CHAR.sql
@@ -4,3 +4,15 @@ select sum(amount),
 TO_CHAR(payment_date, 'YYYY-Month-Day') as modi
 from payment
 group by modi
+
+
+
+--Challenge question for to_char:
+--You need to sum payments and group in the following formats:
+--sum, month, year
+--sum, weekday, time
+--Group them pair by pair
+
+--sum, Day, date
+select sum(amount), to_char( payment_date, 'Day-DD-MM-YYYY') as p_day from payment
+group by p_day
